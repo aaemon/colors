9b475ddd7f6b61794c1b1ae4dc212484f062d144
aaemon <aae.emon96@gmail.com>
2020-04-22 23:15:53 +0600

    initial fix

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const app = express();
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
+  console.log('SERVER IS LISTENING >>>');
+});
